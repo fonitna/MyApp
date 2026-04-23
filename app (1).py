5c1c68443f6d525056ecda19a9aa995f0feebb86
fonitna <44429010+fonitna@users.noreply.github.com>
2026-04-23 23:06:48 +0700

    Add files via upload

---
+++ app (1).py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Multi-page App", layout="wide")
+
+st.title("🏠 หน้าหลัก (Homepage)")
+st.write("ยินดีต้อนรับเข้าสู่เว็บไซต์ที่สร้างด้วย Streamlit บน Colab ครับ")
+
+st.info("👈 คุณสามารถคลิกเลือกหน้าที่ 2 ได้ที่แถบเมนูด้านซ้ายมือ")
+
+if st.button("คลิกเพื่อดูความลับ"):
+    st.balloons()
+    st.success("ยินดีด้วย! คุณเจอความลับแล้ว")
