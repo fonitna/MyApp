902616a70b1decc1e51a411ae4905f2f840b4a80
fonitna <44429010+fonitna@users.noreply.github.com>
2026-04-23 23:43:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="My Multi-page App", layout="wide")
 st.title("🏠 หน้าหลัก (Homepage)")
 st.write("Boot Camp: Data Science and Machine Learning 7 Day Intensive Hands-on Workshop")
 
-st.info("👈 คุณสามารถคลิกเลือกหน้าที่ 2 ได้ที่แถบเมนูด้านซ้ายมือ")
+st.info("Day 1: การจัดการข้อมูลพื้นฐานและโครงสร้างข้อมูลด้วย Python")
 
 if st.button("คลิกเพื่อดูความลับ"):
     st.balloons()
