d60792c37b82f0d477d0841d47c8bb88867a2854
fonitna <44429010+fonitna@users.noreply.github.com>
2026-04-23 23:50:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="My Multi-page App", layout="wide")
 
-st.title("🏠 หน้าหลัก (Homepage)")
+st.title("🏠 หน้าหลัก")
 st.write("### **Boot Camp:** Data Science and Machine Learning")
 st.write("##### 7 Day Intensive Hands-on Workshop")
 st.info("**Day 1:** การจัดการข้อมูลพื้นฐานและโครงสร้างข้อมูลด้วย Python")
