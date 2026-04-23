ecb40d18640a47ebd042c0cab57e324399889a9d
fonitna <44429010+fonitna@users.noreply.github.com>
2026-04-23 23:48:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.set_page_config(page_title="My Multi-page App", layout="wide")
 
 st.title("🏠 หน้าหลัก (Homepage)")
-st.write("Boot Camp: Data Science and Machine Learning 7 Day Intensive Hands-on Workshop")
-
+st.write("### **Boot Camp:** Data Science and Machine Learning")
+st.write("##### 7 Day Intensive Hands-on Workshop")
 st.info("**Day 1:** การจัดการข้อมูลพื้นฐานและโครงสร้างข้อมูลด้วย Python")
 
 if st.button("คลิกเพื่อดูความลับ"):
