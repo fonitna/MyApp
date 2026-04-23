1bc4680eab5a0a7a28321b4b94b878f26d942ce9
fonitna <44429010+fonitna@users.noreply.github.com>
2026-04-23 23:56:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,5 @@ st.write("### Boot Camp: Data Science and Machine Learning")
 st.info("7 Day Intensive Hands-on Workshop")
 st.write("##### Day 1: การจัดการข้อมูลพื้นฐานและโครงสร้างข้อมูลด้วย Python")
 
-if st.button("คลิกเพื่อดูความลับ"):
-    st.balloons()
-    st.success("ยินดีด้วย! คุณเจอความลับแล้ว")
-
-if st.button("ไปที่หน้าที่สอง"):
+if st.button("ระบบคำนวณส่วนลดตามยอดซื้อ"):
     st.switch_page("pages/second_page.py")
