87200e5ebfb97dfa19f36e7f041d72461904feab
fonitna <44429010+fonitna@users.noreply.github.com>
2026-04-23 23:53:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.set_page_config(page_title="My Multi-page App", layout="wide")
 
 st.title("🏠 หน้าหลัก")
-st.write("### **Boot Camp:** Data Science and Machine Learning")
+st.write("### Boot Camp: Data Science and Machine Learning")
 st.write("##### 7 Day Intensive Hands-on Workshop")
-st.write("### **Day 1:** การจัดการข้อมูลพื้นฐานและโครงสร้างข้อมูลด้วย Python")
+st.write("#### Day 1: การจัดการข้อมูลพื้นฐานและโครงสร้างข้อมูลด้วย Python")
 
 if st.button("คลิกเพื่อดูความลับ"):
     st.balloons()
